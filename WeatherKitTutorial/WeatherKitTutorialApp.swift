c963baf1cead8ff1e6748e32b025446105fc2e84
NJuGau <nathanael.gauthama@binus.ac.id>
2024-07-23 16:07:46 +0700

    Initial Commit

---
+++ WeatherKitTutorial/WeatherKitTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherKitTutorialApp.swift
+//  WeatherKitTutorial
+//
+//  Created by Nathanael Juan Gauthama on 23/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherKitTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
